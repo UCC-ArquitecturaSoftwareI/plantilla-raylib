fd73e4b6a437b579a87b8cf1b1e28dcd85fe1b4c
TEST <rmarku@gmail.com>
2020-03-21 18:16:27 -0300

    Agregada información readme.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
+![Build and Deploy][badge]
 # Plantilla de proyecto raylib con CI
 
 Este proyecto es una plantilla básica para crear proyectos en Raylib. Incluye github actions
 para deployear la versión HTML5 del juego en gh-pages del mismo repositorio.
+
+Se puede ver desde: [Github Pages][gh-pages]
+
+
+[gh-pages]:https://ucc-arquitecturasoftwarei.github.io/plantilla-raylib/
+[badge]:https://github.com/UCC-ArquitecturaSoftwareI/plantilla-raylib/workflows/Build%20and%20Deploy/badge.svg
--- main.cpp
+++ main.cpp
@@ -8,55 +8,69 @@
 const int screenWidth = 800;
 const int screenHeight = 450;
 
+// Variables Globales
 Music music;
 Nave *player;
 
-static void UpdateDrawFrame(void);          // Update and Draw one frame
+static void UpdateDrawFrame(void);          // Función dedicada a operar cada frame
 
 int main() {
+    // Inicialización de la ventana
     InitWindow(screenWidth, screenHeight, "raylib template - advance game");
     InitAudioDevice();              // Initialize audio device
 
+    /// Ejemplo de utilización de audio.
     music = LoadMusicStream("resources/Cyberpunk Moonlight Sonata.mp3");
 
     PlayMusicStream(music);
     player = new Nave("resources/ship.png", Vector2{screenWidth / 2, screenHeight / 2});
 
 
-#if defined(PLATFORM_WEB)
+#if defined(PLATFORM_WEB)  // Para versión Web.
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
     SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    // Main loop
+    while (!WindowShouldClose()) {
         UpdateDrawFrame();
     }
 #endif
 
-    UnloadMusicStream(music);   // Unload music stream buffers from RAM
-    CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
+
+    // Descargar todos los resources cargados
+
+    UnloadMusicStream(music);   // Descargo la musica de RAM
+    CloseAudioDevice();         // Cierro el dispositivo de Audio
+    CloseWindow();              // Cierro la ventana
     return 0;
 }
 
+
+/**
+ *  Función dedicada a dibujar cada frame. Acá adentro se debe poner la logica necesaria para representar un nuevo frame
+ *  del juego.
+ */
 static void UpdateDrawFrame(void) {
-    // Allways play music
+
+    // siempre hay que reproducir la musica que esta actualmente
     UpdateMusicStream(music);
 
-    // Verify input
+    // Verifico Entradas de eventos.
     if (IsKeyDown(KEY_RIGHT)) player->move_x(2.0f);
     if (IsKeyDown(KEY_LEFT)) player->move_x(-2.0f);
     if (IsKeyDown(KEY_UP)) player->move_y(-2.0f);
     if (IsKeyDown(KEY_DOWN)) player->move_y(2.0f);
 
 
+    // Comienzo a dibujar
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
+    ClearBackground(RAYWHITE); // Limpio la pantalla con blanco
 
+    // Dibujo todos los elementos del juego.
     player->draw();
-
     DrawText("Inicio", 20, 20, 40, LIGHTGRAY);
 
+    // Finalizo el dibujado
     EndDrawing();
 }
